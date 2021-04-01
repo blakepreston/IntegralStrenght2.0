2afd4250fae7c2b0540f666e3320f5c2bff805ea
Blake <blake.p.preston@gmail.com>
2021-03-31 22:00:21 -0500

    Made Mobile Nav

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-// let menuOpen = document.getElementById('menubtn');
+// let menu = document.getElementById('menu');
+// let button = document.getElementById('menuButton');
 
-function openMenu(){
+function move(){
+    document.getElementById('menu').style.marginLeft = "0px";
+    document.getElementById('menu').style.transitionDuration = "1s";
+}
+
+function menuChange(){
     if(document.getElementById('menubtn').src.match("menu.png")){
         document.getElementById('menubtn').src = "cancel.png";
+    }else if(document.getElementById('menubtn').src.match("cancel.png")){
+        document.getElementById('menubtn').src = "menu.png";
+        document.getElementById('menu').style.marginLeft = "-100%";
+        document.getElementById('menu').style.transitionDuration = "1s";
     }else{
         document.getElementById('menubtn').src = "menu.png";
     }
+}
+
+function closeMenu(){
+    if(document.getElementById('closeMenu')){
+        document.getElementById('menubtn').src = "menu.png";
+        document.getElementById('menu').style.marginLeft = "-100%";
+        document.getElementById('menu').style.transitionDuration = "1s";
+    }
+}
+
+function menuAnimation(){
+    move();
+    menuChange();
 }
